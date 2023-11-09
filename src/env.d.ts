ec6b8f2a7f1f963be57a80d0c26eb40d1be4ce34
kavehrafie <kavehrafie@gmail.com>
2023-11-09 14:57:10 -0600

    - on netlify 6

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-import type {Languages} from "lib/i18n.ts";
+
 
 declare namespace App {
   interface Locals {
-    locale: Languages | undefined | any;
+    locale: string;
     visitorCountry: string;
     hasEdgeMiddleware: boolean;
   }
