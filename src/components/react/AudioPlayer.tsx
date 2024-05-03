5aedb6938fe89eaad907a1e755a4a9f0c7a400b6
kavehrafie <kavehrafie@gmail.com>
2024-05-03 00:27:13 -0500

    hours-min-sec

--- src/components/react/AudioPlayer.tsx
+++ src/components/react/AudioPlayer.tsx
@@ -68,10 +68,13 @@ export default function AudioPlayer({
   };
 
   function formatDuration(durationSeconds: number) {
-    const minutes = Math.floor(durationSeconds / 60);
+    const hours = Math.floor(durationSeconds / 3600);
+    const minutes = Math.floor((durationSeconds % 3600) / 60);
     const seconds = Math.floor(durationSeconds % 60);
     const formattedSeconds = seconds.toString().padStart(2, "0");
-    return `${minutes}:${formattedSeconds}`;
+    return hours > 0
+      ? `${hours}:${minutes}:${formattedSeconds}`
+      : `${minutes}:${formattedSeconds}`;
   }
 
   function handleMute() {
